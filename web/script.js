af20246064e2e3983165ee03b36f83854a39bc40
Jakub Serafin <jakubserafin01@gmail.com>
2024-06-12 17:08:54 +0200

    front adjustments

--- web/script.js
+++ web/script.js
@@ -62,7 +62,7 @@ function block_output_name() {
     outputName.style.cursor = 'not-allowed';
     outputName.value = '';
     outputName.placeholder = 'The backtest has not finished yet';
-    block_save_button()
+    // block_save_button()
 }
 
 eel.expose(unlock_output_name);
@@ -75,7 +75,7 @@ function unlock_output_name() {
     unblock_save_button()
 }
 
-block_save_button()
+// block_save_button()
 
 function unblock_save_button()
 {
@@ -85,13 +85,14 @@ function unblock_save_button()
     saveButton.style.backgroundColor = '#5cb85c'; // Zmień kolor na zielony lub inny kolor wskazujący na aktywność
 }
 
-function block_save_button()
-{
-    const saveButton = document.getElementById('save');
-    saveButton.setAttribute('disabled', true);
-    saveButton.style.cursor = 'not-allowed';
-    saveButton.style.backgroundColor = '#d9534f'; // Zmień kolor na czerwony lub inny kolor wskazujący na brak aktywności
-}
+// function block_save_button()
+// {
+//     const saveButton = document.getElementById('save');
+//     saveButton.setAttribute('disabled', true);
+//     saveButton.style.cursor = 'not-allowed';
+//     saveButton.style.backgroundColor = '#d9534f'; // Zmień kolor na czerwony lub inny kolor wskazujący na brak aktywności
+// }
+
 
 
 function changeText() {
